f022d95eedf79adef488228003ec301099fa31fe
NishaFarooq <farooqnisha086@gmail.com>
2024-04-01 01:43:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const retort = await inquirer.prompt([
+    {
+        message: "Enter your firstValue here:",
+        type: "number",
+        name: "firstValue",
+    },
+    {
+        message: "Enter your secondValue here:",
+        type: "number",
+        name: "secondValue",
+    },
+    {
+        message: "Select any one of them.",
+        type: "list",
+        choices: ["Addition", "Subraction", "Multiplication", "Division"],
+        name: "operator",
+    },
+]);
+console.log(retort);
+if (retort.operator === "Subraction") {
+    console.log(retort.firstValue - retort.secondValue);
+}
+else if (retort.operator === "Addition") {
+    console.log(retort.firstValue + retort.secondValue);
+}
+else if (retort.operator === "Multiplication") {
+    console.log(retort.firstValue * retort.secondValue);
+}
+else if (retort.operator === "Division") {
+    console.log(retort.firstValue / retort.secondValue);
+}
+else {
+    console.log("There is an error, Please! try again.");
+}
